217b4cc9d4d3f6fc7b4b1669138607fc7a1fa564
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-06-15 07:41:21 +0000

    Adding the name in the portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,8 +24,8 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
+  name: "Aditya Kumar",
+  title: "Web Developer & Content Creator",
   email: "alex@example.com",
   gitHub: "microsoft",
   instagram: "microsoft",
